b92df7f6b0c3fe58f49a276783acc21eb66a0728
Bingster12345 <67985688+Bingster12345@users.noreply.github.com>
2024-11-22 23:17:02 -0500

    Update PayReport.py

--- PayReport.py
+++ PayReport.py
@@ -1,1 +1,18 @@
+class PayReport:
+    def __init__(self, caregivers):
+        self.caregivers = caregivers
 
+    def generate_report(self):
+        """Generate a text report of weekly pay."""
+        report = "Weekly Pay Report\n"
+        report += "-" * 20 + "\n"
+        total_pay = 0
+        for caregiver in self.caregivers:
+            pay = caregiver.calculate_pay()
+            total_pay += pay
+            # Using string concatenation
+            report += caregiver.name + ": $" + str(round(pay, 2)) + "\n"
+          
+        report += "-" * 20 + "\n"
+        report += "Total Weekly Pay: $" + str(round(total_pay, 2)) + "\n"
+        return report
